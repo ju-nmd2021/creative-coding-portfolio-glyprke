26f8aa3a32d1f2b28fe8431f63c9bbea20270726
glyprke <gued20ac@student.ju.se>
2023-09-01 21:54:49 +0200

    creatin multiple shapes using for loop

--- experiments/example2.js
+++ experiments/example2.js
@@ -1,29 +1,38 @@
 let size= 250;
 let lineAmount = 150;
+let xPosition = 0;
+let yPosition = 0;
+
 
 
 function setup() {
-  createCanvas(400,400);
-
+  createCanvas(500,500);
 }
 
 function draw() {
   
+  for (let i = 0; i < 10; i++){
+      xPosition = xPosition + 100
+      shape()
+  }
+      
+}
+
+function shape(){
+  
   // random colors of the ellipse
-  fill(random(255), random(150), random(165));
-  noLoop();
-  
-  ellipse(250,250,size,size);
+  //fill(random(255), random(150), random(165));
+  ellipse(xPosition, yPosition,size,size);
   fill(255);
-  
   push();
+  
   //lines inside of the ellipse
-  translate(250,250);
+  translate(xPosition, yPosition);
+  
   // inspired by https://www.youtube.com/watch?v=Efnl8kPL6Kw
   for (let i = 0; i < lineAmount; i ++){
     line(0, 0, 0, size/2);
     rotate(360 /8);
   }
   pop();
-      
 }
