132517f065f03a29d161bff4973b53ccb9a64477
glyprke <gued20ac@student.ju.se>
2023-09-01 16:47:32 +0200

    ellipse and lines

--- experiments/example2.js
+++ experiments/example2.js
@@ -1,18 +1,29 @@
-let positionX = 20;
-let positionY = 20;
-let width = 30;
-let height =30;
+let size= 250;
+let lineAmount = 150;
+
 
 function setup() {
   createCanvas(400,400);
-  
+
 }
 
 function draw() {
-  background(250,230,220)
   
-  fill(random(255), random(255), random(255));
-  rect(positionX, positionY,width, height)
+  // random colors of the ellipse
+  fill(random(255), random(150), random(165));
   noLoop();
   
+  ellipse(250,250,size,size);
+  fill(255);
+  
+  push();
+  //lines inside of the ellipse
+  translate(250,250);
+  // inspired by https://www.youtube.com/watch?v=Efnl8kPL6Kw
+  for (let i = 0; i < lineAmount; i ++){
+    line(0, 0, 0, size/2);
+    rotate(360 /10);
+  }
+  pop();
+      
 }
