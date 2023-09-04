fe74e8c83e859a69fe70a6f84aff904745020fa2
glyprke <gued20ac@student.ju.se>
2023-09-04 19:16:24 +0200

    updating the page

--- experiments/example.js
+++ experiments/example.js
@@ -1,5 +1,8 @@
 
-let circleSize;
+let size= 250;
+let lineAmount = 150;
+let xPosition = 0;
+let yPosition = 100;
 let randomWeight;
 
 function setup() {
@@ -7,17 +10,34 @@ function setup() {
     
 }
 
-function draw() {
-  background(0);
+function shape(){
+  
+  // random colors of the ellipse
+  fill(random(255), random(150), random(165));
+  ellipse(xPosition, yPosition,size,size);
+  fill(255);
   push();
   
-  circleSize = random(5,50);
-  randomWeight = random(1,5);
+  //lines inside of the ellipse
+  translate(xPosition, yPosition);
   
-  strokeWeight(randomWeight);
-  stroke(255);
-  fill(255);
-  circle(50, 200,circleSize);
+  // inspired by https://www.youtube.com/watch?v=Efnl8kPL6Kw
+  for (let i = 0; i < lineAmount; i ++){
+    line(0, 0, 0, size/2);
+    rotate(360 /8);
+  }
+
   pop();
-    
+}
+
+function draw() {
+  
+  for (let i = 0; i < 10; i++){
+      xPosition = xPosition + 100
+    for (let k = 0; k < 10; k++){
+       shape()
+       yPosition = yPosition + 100
+    }   
+    yPosition =100;
+  }  
 }
