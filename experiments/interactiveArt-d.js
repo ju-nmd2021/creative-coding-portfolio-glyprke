7e105b9017a4d66441ce7040c5c4ab973015af8b
glyprke <gued20ac@student.ju.se>
2023-09-07 12:06:57 +0200

    interactive art project

---
+++ experiments/interactiveArt-d.js
@@ -0,0 +1,27 @@
+let inc = 0.010;
+let start = 0.01;
+
+
+function setup() {
+  createCanvas(innerWidth, innerHeight);
+}
+
+// inspired by https://editor.p5js.org/codingtrain/sketches/EZeHXBhei
+
+function draw() {
+  background(50);
+  
+  noFill();
+  beginShape();
+  let xoff = start;
+  for (let x = 0; x < width; x++) {
+    stroke(280, 50);
+    let y = noise(xoff) * width ;
+    line(x, y, width / 2, height /2);
+    xoff += inc;
+  }
+  endShape();
+
+  start += inc;
+  
+}
